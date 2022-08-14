9f9739941f9d9efdf6e2d7b1bf4b2d59847bbcd1
danangoffic <darifrahmanda@gmail.com>
2022-08-14 08:45:08 +0700

    fetch user logged endpoint

--- handler/user.go
+++ handler/user.go
@@ -185,3 +185,13 @@ func (h *userHandler) UploadAvatar(c *gin.Context) {
 	c.JSON(http.StatusOK, response)
 
 }
+
+func (h *userHandler) FetchUser(c *gin.Context) {
+	currentUser := c.MustGet("currentUser").(user.User)
+
+	formatter := user.FormatUser(currentUser, "")
+
+	response := helper.APIResponse("Successfully fetch user data", http.StatusOK, "success", formatter)
+
+	c.JSON(http.StatusOK, response)
+}
--- main.go
+++ main.go
@@ -62,6 +62,7 @@ func main() {
 	api.POST("/sessions", userHandler.Login)
 	api.POST("/email_checker", userHandler.CheckEmailAvailability)
 	api.POST("/avatars", authMiddleware(authService, userService), userHandler.UploadAvatar)
+	api.GET("/users/fetch", authMiddleware(authService, userService), userHandler.FetchUser)
 
 	// CAMPAIGNS API
 	api.POST("/campaigns", authMiddleware(authService, userService), campaignHandler.CreateCampaign)
